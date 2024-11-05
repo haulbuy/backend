2a1aea3cf49194ff6407a94dbf21a9fcdeeba2df
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2024-11-04 23:55:29 -0600

    Unused vars change explicit comment

--- src/features/orders/controller.ts
+++ src/features/orders/controller.ts
@@ -1,4 +1,4 @@
-// deno-lint-ignore-file no-explicit-any
+// deno-lint-ignore-file no-explicit-any no-unused-vars
 
 import { Context } from "https://deno.land/x/oak@v10.0.0/mod.ts";
 import { createOrders } from "./service.ts";
