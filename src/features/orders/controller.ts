b5ea9362c5d761697ff30c659093d6ebe445d096
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2024-11-04 23:51:46 -0600

    Finalized the controller with the new auth features

--- src/features/orders/controller.ts
+++ src/features/orders/controller.ts
@@ -1,12 +1,18 @@
+// deno-lint-ignore-file no-explicit-any
+
 import { Context } from "https://deno.land/x/oak@v10.0.0/mod.ts";
 import { createOrders } from "./service.ts";
+import { getUserIdFromToken } from "../../utils/authUtils.ts";
 
 export const create = async (ctx: Context) => {
     try {
+        const userId = await getUserIdFromToken(ctx);
+        if (!userId) return;
+
         const { cartItems } = await ctx.request.body().value;
 
         try {
-            const msg = createOrders(cartItems)
+            const msg = await createOrders(userId, cartItems)
 
             ctx.response.status = 200;
             ctx.response.body = { message: msg }
@@ -14,10 +20,6 @@ export const create = async (ctx: Context) => {
             ctx.response.status = 500;
             ctx.response.body = { error: err.message };
         }
-
-        ctx.response.status = 200;
-        ctx.response.body = 
-
     } catch (err: any) {
         ctx.response.status = 400;
         ctx.response.body = { err: "Invalid request data " };
