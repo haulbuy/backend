f3902fe4c00f231d128da0c68d7c5244dbbc00cb
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2024-11-05 17:41:52 -0600

    Add route for order processing

--- src/features/orders/routes.ts
+++ src/features/orders/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "../../../deps.ts";
 
-import { create } from "./controller.ts";
+import { create, process } from "./controller.ts";
 
 const router = new Router();
 
 router.post("/orders/create", create);
+router.post("/orders/process", process);
 
 export default router;
