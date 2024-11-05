57640037036a509eca8e2047412bca2e98190094
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2024-11-04 18:53:47 -0600

    Added comments to deps.ts for clarity

--- db/supabaseClient.ts
+++ db/supabaseClient.ts
@@ -1,8 +1,14 @@
-import { config, createClient } from "../deps.ts";
+import { config } from "../deps.ts";
+import { createClient } from "../deps.ts";
 
 const { SUPABASE_URL,SUPABASE_ANON_KEY, SUPABASE_SERVICE_WORKER_KEY } = config();
 
-export const supabase = createClient(
+export const supabaseClient = createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
+)
+
+export const supabaseServiceClient = createClient(
+    SUPABASE_URL,
+    SUPABASE_SERVICE_WORKER_KEY
 )
