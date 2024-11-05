8d4c3bb6560ff9fa095ace80d5b8a7c89d2068de
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2024-11-04 18:44:15 -0600

    Added database.ts (begun) and fixed common convention with strings

---
+++ db/database.ts
@@ -0,0 +1,2 @@
+import { Client } from "https://deno.land/x/postgres@v0.19.3/mod.ts";
+import { config } from "../deps.ts";
--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { Application, Router } from 'https://deno.land/x/oak@v10.0.0/mod.ts';
+export { Application, Router } from "https://deno.land/x/oak@v10.0.0/mod.ts";
 export { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
