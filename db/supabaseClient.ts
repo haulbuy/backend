52068b4eb87e5a88a1e97b7506ff708d06f8984a
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2024-11-04 18:54:36 -0600

    Finalized supabaseClient.ts, added comments for clarification

--- db/supabaseClient.ts
+++ db/supabaseClient.ts
@@ -3,11 +3,13 @@ import { createClient } from "../deps.ts";
 
 const { SUPABASE_URL,SUPABASE_ANON_KEY, SUPABASE_SERVICE_WORKER_KEY } = config();
 
+// Standard client for regular autthenticated requests
 export const supabaseClient = createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 )
 
+// Service client for server-side elevated permissions
 export const supabaseServiceClient = createClient(
     SUPABASE_URL,
     SUPABASE_SERVICE_WORKER_KEY
