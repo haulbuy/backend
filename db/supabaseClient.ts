483a4d69147d9bc5df3cfd5052b31569ba59dfea
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2024-11-06 01:58:32 -0600

    Refactor Supabase client configuration to use environment variables for keys

--- db/supabaseClient.ts
+++ db/supabaseClient.ts
@@ -1,16 +1,17 @@
-import { config } from "../deps.ts";
 import { createClient } from "../deps.ts";
 
-const { SUPABASE_URL, SUPABASE_ANON_KEY, SUPABASE_SERVICE_WORKER_KEY } = config();
+const SUPABASE_URL = Deno.env.get("SUPABASE_URL") || "";
+const SUPABASE_ANON_KEY = Deno.env.get("SUPABASE_ANON_KEY") || "";
+const SUPABASE_SERVICE_WORKER_KEY = Deno.env.get("SUPABASE_SERVICE_WORKER_KEY") || "";
 
-// Standard client for regular autthenticated requests
+// Standard client for regular authenticated requests
 export const supabase = createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
-)
+);
 
 // Service client for server-side elevated permissions
 export const supabaseServiceClient = createClient(
     SUPABASE_URL,
     SUPABASE_SERVICE_WORKER_KEY
-)
+);
--- deps.ts
+++ deps.ts
@@ -1,6 +1,5 @@
 // Deno
 export { Application, Router } from "https://deno.land/x/oak@v10.0.0/mod.ts";
-export { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
 
 // Supabase
 export { createClient } from "https://esm.sh/@supabase/supabase-js@2.46.1";
