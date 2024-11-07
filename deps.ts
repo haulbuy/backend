35c3f4a95e7b812279c7952dbb2332005eec1768
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2024-11-07 13:47:47 -0600

    Remove unused imports and user creation service code

--- deps.ts
+++ deps.ts
@@ -3,6 +3,3 @@ export { Application, Router } from "https://deno.land/x/oak@v10.0.0/mod.ts";
 
 // Supabase
 export { createClient } from "https://esm.sh/@supabase/supabase-js@2.46.1";
-
-// Utility
-import { validate, required, minLength, isString } from "https://deno.land/x/validasaur@v0.15.0/mod.ts";
