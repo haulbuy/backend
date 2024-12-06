15e49a6d5af745712aadfada696c87829a0b297c
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2024-12-05 18:41:49 -0600

    Remove calculateShipping function and its route from shipping controller

--- src/features/shipping/routes.ts
+++ src/features/shipping/routes.ts
@@ -1,8 +1,5 @@
 import { Router } from "../../../deps.ts";
-import { calculateShipping } from "./controller.ts";
 
 const router = new Router();
-
-router.post("/shipping/calculate", calculateShipping);
 
 export default router;
